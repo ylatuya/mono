0db6382badcdb8d0f96c6dab3e5b0f5a0f1e9dcb
Andoni Morales Alastruey <ylatuya@gmail.com>
2016-09-23 12:51:43 +0200

    QIP

---
+++ mcs/class/System.ServiceModel.Discovery/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace test
+{
+	[TestFixture()]
+	public class Test
+	{
+		[Test()]
+		public void TestCase()
+		{
+		}
+	}
+}
--- mcs/class/System.ServiceModel.Discovery/Test/System.ServiceModel.Discovery/DiscoveryClientTest.cs
+++ mcs/class/System.ServiceModel.Discovery/Test/System.ServiceModel.Discovery/DiscoveryClientTest.cs
@@ -26,12 +26,18 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
 using System.ServiceModel;
 using System.ServiceModel.Channels;
 using System.ServiceModel.Description;
 using System.ServiceModel.Discovery;
 using System.ServiceModel.Dispatcher;
+using System.Xml;
+using MonoTests.Helpers;
 using NUnit.Framework;
+using System.Text;
 
 namespace MonoTests.System.ServiceModel.Discovery
 {
@@ -39,26 +45,62 @@ namespace MonoTests.System.ServiceModel.Discovery
 	public class DiscoveryClientTest
 	{
 		[Test]
-		public void ContractInterfaceManaged ()
+		public void ContractInterfaceManaged()
 		{
-			var client = new DiscoveryClient (new DiscoveryEndpoint ());
+			var client = new DiscoveryClient(new DiscoveryEndpoint());
 			var v11 = client.ChannelFactory.Endpoint;
-			Assert.IsNotNull (v11, "v11");
-			Assert.AreEqual ("DiscoveryProxy", v11.Name, "v11.Name");
-			Assert.AreEqual (2, v11.Contract.Operations.Count, "v11.Operations.Count");
-			Assert.IsNull (v11.Contract.CallbackContractType, "v11.CallbackContractType");
+			Assert.IsNotNull(v11, "v11");
+			Assert.AreEqual("DiscoveryProxy", v11.Name, "v11.Name");
+			Assert.AreEqual(2, v11.Contract.Operations.Count, "v11.Operations.Count");
+			Assert.IsNull(v11.Contract.CallbackContractType, "v11.CallbackContractType");
 		}
 
 		[Test]
-		public void ContractInterfaceAdhoc ()
+		public void ContractInterfaceAdhoc()
 		{
-			var client = new DiscoveryClient (new UdpDiscoveryEndpoint ());
+			var client = new DiscoveryClient(new UdpDiscoveryEndpoint());
 			var v11 = client.ChannelFactory.Endpoint;
-var cd = ContractDescription.GetContract (v11.Contract.ContractType);
-			Assert.IsNotNull (v11, "v11");
-			Assert.AreEqual ("CustomBinding_TargetService", v11.Name, "v11.Name");
-			Assert.AreEqual (5, v11.Contract.Operations.Count, "v11.Operations.Count");
-			Assert.IsNotNull (v11.Contract.CallbackContractType, "v11.CallbackContractType");
+			var cd = ContractDescription.GetContract(v11.Contract.ContractType);
+			Assert.IsNotNull(v11, "v11");
+			Assert.AreEqual("CustomBinding_TargetService", v11.Name, "v11.Name");
+			Assert.AreEqual(5, v11.Contract.Operations.Count, "v11.Operations.Count");
+			Assert.IsNotNull(v11.Contract.CallbackContractType, "v11.CallbackContractType");
+		}
+
+		[Test]
+		public void TestClientDiscovery()
+		{
+			/*UdpClient server = new UdpClient(3702);
+			server.JoinMulticastGroup(IPAddress.Parse("239.255.255.250"));
+			IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+			server.ReceiveAsync().ContinueWith(r =>
+			{
+				string response = @"<?xml version='1.0' encoding='utf-8'?>
+					<s:Envelope xmlns:a='http://schemas.xmlsoap.org/ws/2004/08/addressing' xmlns:s='http://www.w3.org/2003/05/soap-envelope'>
+						<s:Header>
+							<a:Action s:mustUnderstand='1'>http://schemas.xmlsoap.org/ws/2005/04/discovery/Probe</a:Action>
+							<a:MessageID>urn:uuid:e77dfa98-1cad-4520-9f1d-2a5c561dcbb8</a:MessageID>
+						</s:Header>
+						<s:Body>
+							<Probe xmlns='http://schemas.xmlsoap.org/ws/2005/04/discovery'>
+								<Duration xmlns='http://schemas.microsoft.com/ws/2008/06/discovery'>PT5S</Duration>
+							</Probe>
+						</s:Body>
+					</s:Envelope>";
+				var bytes = Encoding.ASCII.GetBytes(response);
+				server.Send(bytes, bytes.Length, r.Result.RemoteEndPoint);
+				Console.WriteLine(response);
+			});
+			*/
+
+			UdpDiscoveryEndpoint ude = new UdpDiscoveryEndpoint(DiscoveryVersion.WSDiscoveryApril2005);
+			DiscoveryClient discoveryClient = new DiscoveryClient(ude);
+			FindCriteria findCriteria = new FindCriteria();
+			findCriteria.ContractTypeNames.Add(new XmlQualifiedName("NetworkVideoTransmitter", @"http://www.onvif.org/ver10/network/wsdl"));
+			findCriteria.MaxResults = 1;
+			findCriteria.Duration = TimeSpan.FromSeconds(5);
+			discoveryClient.Find(findCriteria);
+			//server.Close();
 		}
 	}
 }
